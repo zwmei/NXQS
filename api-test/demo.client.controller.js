b779b71bfc7ed81fd6aaff7c02a446327287ae08
zwmei <541149886@qq.com>
2016-05-03 15:54:25 +0800

    Change api test

--- api-test/demo.client.controller.js
+++ api-test/demo.client.controller.js
@@ -4,15 +4,15 @@
 
 $(function () {
   //var serverAddress = 'http://www.zzvan.com/'; //柱柱测试平台
-  //var serverAddress = 'http://localhost:3002/'; //柱柱测试平台
+  var serverAddress = 'http://localhost:3002/'; //柱柱测试平台
   //var serverAddress = 'http://api.zhuzhu56.com/'; //柱柱测试平台
-  var serverAddress = 'http://www.cvs001.com/';
+  //var serverAddress = 'http://www.cvs001.com/';
 
   //zzvan 541149886@qq.com
-  var secret_key = '6f21000b0b3a39d418359d6cebd0ed29cd613567afda29f1c85331c03cd0e49f6da3edbef7a146cbf0845c0ceaf84486'; //私钥, 申请开发者后从柱柱获取
-  var public_key = 'c6bd846d9431e8bbf1680c13b9a24097d6183c4d4c1674572509c3ebe2cd27915a9c4908b48172627a2446fff0a877cd'; //私钥, 申请开发者后从柱柱获取
+  var secret_key = "6f21000b0b3a39d418359d6cebd0ed296ad5b7e38b5218aaa6055c6a1e8931fd5bd3147a5b6fd1efe76ce5174a910051"; //私钥, 申请开发者后从柱柱获取
+  var public_key = "c6bd846d9431e8bbf1680c13b9a24097e63a47ac1250a467465e51f1aa59174a75cc0f447b80b1f61a180b73aa1af726"; //私钥, 申请开发者后从柱柱获取
   var timestamp_A = getTimeString(new Date());
-  var company_id = "55af3b313dc39a7415e8ba93";
+  var company_id = "5726fdec06e50d410a7df353";
   var order_number = 'zhuzhu-demo';
 
   ////localhost 541149886@qq.com
@@ -40,65 +40,222 @@ $(function () {
 
   var driverButton = $('body').find('.driver-button');
   driverButton.click(function () {
-    var driverUrl = serverAddress + 'api/driver/list?'
-      + 'signature=' + signature_A
-      + '&timestamp=' + timestamp_A
-      + '&company_id=' + company_id;
+    //var driverUrl = serverAddress + 'api/driver/list?'
+    //  + 'signature=' + signature_A
+    //  + '&timestamp=' + timestamp_A
+    //  + '&company_id=' + company_id;
+    //
+    //window.open(driverUrl);
 
-    window.open(driverUrl);
-  });
 
-  var detailButton = $('body').find('.detail-button');
-  detailButton.click(function () {
-    var detailUrl = serverAddress + 'api/orderpage?'
-      + 'signature=' + signature_A
-      + '&timestamp=' + timestamp_A
-      + '&company_id=' + company_id
-      + '&order_number=' + order_number;
 
-    window.open(detailUrl);
-  });
+    var createUrl = serverAddress + 'api/delorder1';
 
-  var createButton = $('body').find('.create-button');
-  createButton.click(function () {
-    var createUrl = serverAddress + 'api/multiorder';
+
     $.ajax({
       data: {
-        signature: '0d2df591bb7936d7ee9b7bd71ed0',
-        timestamp: '20150702143030',
+        signature: signature_A,
+        timestamp: timestamp_A,
         company_id: company_id,
-        group_name: 'default_group',
-        order_infos: [{
-          order_number: 'zhuzhu-8801' //值自己修改
-          //其他参数选填，参考文档
-        }, {
-          order_number: 'zhuzhu-8802',
-          goods: [{
-            name: '苹果',
-            count: 44,
-            unit: '公斤',
-            count2: 55,
-            unit2: '吨',
-            count3: 66,
-            unit3: '公斤',
-            price: 100
-          }, {
-            name: '梨子',
-            count: 45,
-            unit: '公斤',
-            price: 101
-          }, {
-            name: '橘子',
-            count: 46,
-            unit: '公斤',
-            price: 102
-          }]
-        }]
+        reference_no:'神州一号'
       },
       type: 'post',
       url: createUrl,
       dataType: 'json'
     }).done(function (data) {
+      console.log(data);
+
+    }).fail(function (err) {
+      console.log(err);
+    });
+
+
+  });
+
+  var detailButton = $('body').find('.detail-button');
+  detailButton.click(function () {
+    //var detailUrl = serverAddress + 'api/orderpage?'
+    //  + 'signature=' + signature_A
+    //  + '&timestamp=' + timestamp_A
+    //  + '&company_id=' + company_id
+    //  + '&order_number=' + order_number;
+    //
+    //window.open(detailUrl);
+    var createUrl = serverAddress + 'api/order/detail/number1';
+
+
+    $.ajax({
+        data: {
+          signature: signature_A,
+          timestamp: timestamp_A,
+          company_id: company_id,
+          reference_no:'神州一号'
+        },
+        type: 'post',
+        url: createUrl,
+        dataType: 'json'
+      }).done(function (data) {
+
+     console.log(data);
+
+    }).fail(function (err) {
+      console.log(err);
+    });
+
+  });
+
+  var createButton = $('body').find('.create-button');
+  createButton.click(function () {
+    var createUrl = serverAddress + 'api/multiorder1';
+    $.ajax({
+      data: {
+        signature: signature_A,
+        timestamp: timestamp_A,
+        company_id: company_id,
+        group_name: 'default_group',
+        order_infos: [
+          {
+            reference: '神州一号',
+            plate_no: '沪B H001S',
+            driver: '王小六',
+            driver_mobile: '13918429709',
+            shipping_date: '2016-06-01',
+            pickup_check: 1,
+            delivery_check: 1,
+            sheet_infos: [
+              {
+                so_reference: '神州一号',
+                reference: '神州一号001',
+                customer_code: '长安民生',
+                shipping_date: '2016-06-01',
+                detail_infos: [
+                  {
+                    so_reference: '神州一号',
+                    ss_reference: '神州一号001',
+                    item_code: '底盘',
+                    item_ident_code: '123456',
+                    order_reference: 'ABCDE',
+                    plan_box: 10,
+                    plan_quantity: 100,
+                    memo: '注意安全'
+                  },
+                  {
+                    so_reference: '神州一号',
+                    ss_reference: '神州一号001',
+                    item_code: '轮胎',
+                    item_ident_code: '654321',
+                    order_reference: 'EDCBA',
+                    plan_box: 1,
+                    plan_quantity: 10,
+                    memo: '注意安全咯'
+                  }
+                ]
+              },
+              {
+                so_reference: '神州一号',
+                reference: '神州二号001',
+                customer_code: '长安民生2',
+                shipping_date: '2016-06-02',
+                detail_infos: [
+                  {
+                    so_reference: '神州一号',
+                    ss_reference: '神州二号001',
+                    item_code: '底盘',
+                    item_ident_code: '123456',
+                    order_reference: 'ABCDE',
+                    plan_box: 10,
+                    plan_quantity: 100,
+                    memo: '注意安全'
+                  },
+                  {
+                    so_reference: '神州一号',
+                    ss_reference: '神州二号001',
+                    item_code: '轮胎',
+                    item_ident_code: '654321',
+                    order_reference: 'EDCBA',
+                    plan_box: 1,
+                    plan_quantity: 10,
+                    memo: '注意安全咯'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            reference: '长江一号',
+            plate_no: '沪B H001S',
+            driver: '王小六',
+            driver_mobile: '13918429709',
+            shipping_date: '2016-06-01',
+            pickup_check: 1,
+            delivery_check: 1,
+            sheet_infos: [
+              {
+                so_reference: '长江一号',
+                reference: '长江一号001',
+                customer_code: '长安民生',
+                shipping_date: '2016-06-01',
+                detail_infos: [
+                  {
+                    so_reference: '长江一号',
+                    ss_reference: '长江一号001',
+                    item_code: '底盘',
+                    item_ident_code: '123456',
+                    order_reference: 'ABCDE',
+                    plan_box: 10,
+                    plan_quantity: 100,
+                    memo: '注意安全'
+                  },
+                  {
+                    so_reference: '长江一号',
+                    ss_reference: '长江一号001',
+                    item_code: '轮胎',
+                    item_ident_code: '654321',
+                    order_reference: 'EDCBA',
+                    plan_box: 1,
+                    plan_quantity: 10,
+                    memo: '注意安全咯'
+                  }
+                ]
+              },
+              {
+                so_reference: '长江一号',
+                reference: '长江二号001',
+                customer_code: '长安民生2',
+                shipping_date: '2016-06-02',
+                detail_infos: [
+                  {
+                    so_reference: '长江一号',
+                    ss_reference: '长江二号001',
+                    item_code: '底盘',
+                    item_ident_code: '123456',
+                    order_reference: 'ABCDE',
+                    plan_box: 10,
+                    plan_quantity: 100,
+                    memo: '注意安全'
+                  },
+                  {
+                    so_reference: '长江一号',
+                    ss_reference: '长江二号001',
+                    item_code: '轮胎',
+                    item_ident_code: '654321',
+                    order_reference: 'EDCBA',
+                    plan_box: 1,
+                    plan_quantity: 10,
+                    memo: '注意安全咯'
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      type: 'post',
+      url: createUrl,
+      dataType: 'json'
+    }).done(function (data) {
+      console.log(data);
+
       if (data.err) {
         alert(data.err);
       }
